ac605b6a6be71c47eedbc26c3fea3503808955b2
Hugh S <61617034+hughsio@users.noreply.github.com>
2024-10-06 03:03:24 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,6 @@
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+		return new Response('Hello World! Welcome to my test!');
 	},
 } satisfies ExportedHandler<Env>;
